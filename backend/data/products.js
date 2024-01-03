274813abcc755239cae5d2f495ee8babe5cbe55f
Travis Davis <skoolboi434@gmail.com>
2024-01-02 23:37:05 -0500

    MongoDB & Mongoose setup

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,10 +1,10 @@
 const products = [
   {
-    _id: '1',
     name: 'Girl Carroll Hello Kitty & Friends Deck',
     image: '/images/Girl-Carroll-Hello-Kitty.png',
     description: 'The Girl Carroll Hello Kitty & Friends Deck 8.0 x 31.875 makes a perfect wall-hanger for your favorite Hello Kitty-fan. You know they will love you forever! Constructed from traditional 7-ply maple wood and includes FREE griptape. Stain colors may vary.',
     brand: 'Girl',
+    category: 'deck',
     size: '8.0 x 31.875',
     price: 60.0,
     countInStock: 10,
@@ -12,11 +12,11 @@ const products = [
     numReviews: 12
   },
   {
-    _id: '2',
     name: 'Girl Pacheco Hello Kitty & Friends Deck',
     image: '/images/Girl-Pacheco-Hello-Kitty.png',
     description: 'The Girl Pacheco Hello Kitty & Friends Deck 8.5 x 32 makes a perfect wall-hanger for your favorite Hello Kitty-fan. You know they will love you forever! Constructed from traditional 7-ply maple wood and includes FREE griptape. Stain colors may vary. ',
     brand: 'Girl',
+    category: 'deck',
     size: '8.5 x 32',
     price: 60.0,
     countInStock: 7,
@@ -24,11 +24,11 @@ const products = [
     numReviews: 8
   },
   {
-    _id: '3',
     name: 'Santa Cruz Classic Dot Black Deck',
     image: '/images/santa-cruz-classic.png',
     description: 'Stick to the classics and go skate with the Santa Cruz Classic Dot Black Deck 8.25 x 31.83. This is a pro quality skateboard deck constructed from traditional 7-ply maple wood with background stains that could be different than what is pictured. FREE griptape is available with the purchase of the deck.',
     brand: 'Santa Cruz',
+    category: 'deck',
     size: '8.25 x 31.83',
     price: 62.95,
     countInStock: 5,
@@ -36,11 +36,11 @@ const products = [
     numReviews: 12
   },
   {
-    _id: '4',
     name: 'Plan B Blue Crypto Deck',
     image: '/images/planb-crypto.png',
     description: 'Hit the streets on the Plan B Blue Crypto Deck 8.0 x 31.33. This is a pro quality skateboard deck constructed from traditional 7-ply maple wood with background stains that could be different than what is pictured. FREE griptape is available with the purchase of the deck.',
     brand: 'Plan B',
+    category: 'deck',
     size: '8.0 x 31.33',
     price: 55.0,
     countInStock: 11,
@@ -48,11 +48,11 @@ const products = [
     numReviews: 12
   },
   {
-    _id: '5',
     name: 'DGK Blossom Crew Forest Deck',
     image: '/images/dgk-blossom.png',
     description: 'Hit the streets on the DGK Blossom Crew Forest Deck 8.0 x 31.875. This is a pro quality skateboard deck constructed of traditional 7-ply maple wood with background stains that may vary. FREE griptape is included with the purchase of the deck.',
     brand: 'DGK',
+    category: 'deck',
     size: '8.0 x 31.875',
     price: 64.99,
     countInStock: 7,
@@ -60,11 +60,11 @@ const products = [
     numReviews: 10
   },
   {
-    _id: '6',
     name: 'Real Classic Oval Deck',
     image: '/images/real-classic.png',
     description: 'Stick to the classics and skate through the streets with the Real Classic Oval Deck 8.12 x 31.38. This is a pro quality skateboard deck constructed from traditional 7-ply maple wood using Real R1 Construction for longer lasting strength, pop, stiffness and concave. Background stains could be different than what is pictured and FREE griptape is available with the purchase of the deck.',
     brand: 'Real',
+    category: 'deck',
     size: '8.12 x 31.38',
     price: 65.0,
     countInStock: 10,
